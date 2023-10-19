81f7d292aee7b9e5978d46fc60f54f8661d8a1e7
Minakshee Bari <minaksheebari22@gmail.com>
2023-10-19 07:24:27 +0530

    Second day of C, Adding relational and sizeof function.

---
+++ relationalOp.c
@@ -0,0 +1,32 @@
+// Working of relational operators
+#include <stdio.h>
+int main()
+{
+    //int a = 5, b = 5, c = 10;
+    
+    //Input from user
+    
+    int a,b,c;
+    
+    printf("Enter first number: ");
+    scanf("%d",&a);
+    printf("Enter second number: ");
+    scanf("%d",&b);
+    printf("Enter third number: ");
+    scanf("%d",&c);
+
+    printf("%d == %d is %d \n", a, b, a == b);
+    printf("%d == %d is %d \n", a, c, a == c);
+    printf("%d > %d is %d \n", a, b, a > b);
+    printf("%d > %d is %d \n", a, c, a > c);
+    printf("%d < %d is %d \n", a, b, a < b);
+    printf("%d < %d is %d \n", a, c, a < c);
+    printf("%d != %d is %d \n", a, b, a != b);
+    printf("%d != %d is %d \n", a, c, a != c);
+    printf("%d >= %d is %d \n", a, b, a >= b);
+    printf("%d >= %d is %d \n", a, c, a >= c);
+    printf("%d <= %d is %d \n", a, b, a <= b);
+    printf("%d <= %d is %d \n", a, c, a <= c);
+
+    return 0;
+}
